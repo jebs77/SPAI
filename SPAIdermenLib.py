448a3e8572820e25dcf1f3843510614eee732b12
Jens Coosemans <79720898+jebs77@users.noreply.github.com>
2023-11-16 23:31:21 +0100

    upload interpolation

---
+++ SPAIdermenLib.py
@@ -0,0 +1,167 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import ot
+import scipy as sp
+
+# Function of the transport plan
+def transport_plan(P, Q):
+    # Convert point lists into numpy arrays
+    P_array = np.array(P)
+    Q_array = np.array(Q)
+    # Calculate the cost matrix, the squared euclidean distance between points
+    cost_matrix = ot.dist(P_array, Q_array, metric='sqeuclidean')
+    
+    # Compute the amount of mass to transport from P to Q
+    a, b = np.ones((len(P),)) / len(P), np.ones((len(Q),)) / len(Q)
+    
+    # Compute the optimal transport matrix using the EMD solver
+    T = ot.emd(a, b, cost_matrix)
+    
+    return T
+
+# Function to construct the interpolated point cloud
+def construct_interpolated_point_cloud(P, Q, T, kappa):
+    # Assuming u and v are given or can be calculated from P and Q
+    u = np.sum(T, axis=1)
+    v = np.sum(T, axis=0)
+    
+    # Initialize the variables
+    K = len(P) + len(Q)  # total number of points after interpolation
+    interpolated_points = []
+    
+    # Copy the transport plan
+    T_prime = np.copy(T)
+    
+    # Start with the algorithm
+    k = 1
+    for i, ui in enumerate(u):
+        if ui > 0:
+            if np.sum(T[:, i]) == 0:  # Check for vanishing point
+                r_k = (1 - kappa) * ui
+                z_k = P[i]
+                interpolated_points.append((r_k, z_k))
+                k += 1
+            else:
+                T_prime[:, i] = T[:, i] + (1 - kappa) * ui * T[:, i] / np.linalg.norm(T[:, i], 1)
+    
+    for j, vj in enumerate(v):
+        if vj > 0:
+            if np.sum(T[j, :]) == 0:  # Check for appearing point
+                r_k = kappa * vj
+                z_k = Q[j]
+                interpolated_points.append((r_k, z_k))
+                k += 1
+            else:
+                T_prime[j, :] = T_prime[j, :] + kappa * vj * T_prime[j, :] / np.linalg.norm(T_prime[j, :], 1)
+    
+    for i in range(len(P)):
+        for j in range(len(Q)):
+            if T_prime[i, j] > 0:  # Check for moving point
+                r_k = T_prime[i, j]
+                z_k = (1 - kappa) * P[i] + kappa * Q[j]
+                interpolated_points.append((r_k, z_k))
+                k += 1
+    
+    # Return the interpolated point cloud
+    return np.array([z for r, z in interpolated_points])
+    # You should already have your P, Q, and T defined here, as well as the value for kappa.
+    # interpolated_point_cloud = construct_interpolated_point_cloud(P, Q, T, kappa)
+
+# Function to generate random points with pressure
+def generate_random_sources(num_sources):
+    positions = np.random.rand(num_sources, 3) * 5  # random positions within a 5x5x5 cube
+    pressures = np.random.rand(num_sources)  # random pressures between 0 and 1
+    return [{'position': tuple(pos), 'pressure': pressure} for pos, pressure in zip(positions, pressures)]
+
+def calculate_cloud_position(P, Q):
+    P_positions = np.array([list(d['position']) for d in P])
+    Q_positions = np.array([list(d['position']) for d in Q])
+    return P_positions, Q_positions
+
+def calculate_cloud_pressure(P, Q):
+    P_pressures = np.array([d['pressure'] for d in P])
+    Q_pressures = np.array([d['pressure'] for d in Q])
+    return P_pressures, Q_pressures
+
+def visualize_interpolation(P, Q, T, kappa):
+    # Define the virtual sources for P and Q as numpy arrays
+    P_positions, Q_positions = calculate_cloud_position(P, Q)
+
+    # Define the pressures as arrays
+    P_pressures, Q_pressures = calculate_cloud_pressure(P, Q)
+
+    # Construct the interpolated point cloud
+    interpolated_point_cloud = construct_interpolated_point_cloud(P_positions, Q_positions, T, kappa)
+
+    # Plot the original and interpolated point clouds
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Plot the original P point cloud with label for legend
+    ax.scatter(P_positions[:, 0], P_positions[:, 1], P_positions[:, 2], s=P_pressures * 100, c='r', marker='o', label='Original P')
+
+    # Plot the original Q point cloud with label for legend
+    ax.scatter(Q_positions[:, 0], Q_positions[:, 1], Q_positions[:, 2], s=Q_pressures * 100, c='b', marker='^', label='Original Q')
+
+    # Plot the interpolated point cloud with label for legend
+    ax.scatter(interpolated_point_cloud[:, 0], interpolated_point_cloud[:, 1], interpolated_point_cloud[:, 2], s=50, c='g', marker='x', label='Interpolated')
+
+    ax.set_xlabel('X axis')
+    ax.set_ylabel('Y axis')
+    ax.set_zlabel('Z axis')
+    ax.set_title('Original and Interpolated Point Clouds')
+
+    # Add legend to the plot
+    ax.legend(loc='upper left', bbox_to_anchor=(1.05, 1), borderaxespad=0.)
+
+    # Adjust layout to make room for the legend
+    plt.tight_layout()
+
+    plt.show()
+
+def delta_impulse(t, tau, fs):
+    # Create a delta impulse function for discrete time
+    # t is the time array, tau is the time at which the impulse occurs, fs is the sampling rate
+    return np.where(np.isclose(t, tau, atol=1/fs), 1.0, 0.0)
+
+def calculate_omnidirectional_RIR(P, c, fs, max_time):
+    # fs is the sampling rate
+    # max_time is the maximum time for the RIR in seconds
+
+    # Define the time vector with a time step of 1/fs
+    t = np.arange(0, max_time, 1/fs)
+    
+    # Initialize the RIR to zero
+    h_t = np.zeros_like(t)
+
+    # Calculate RIR as superposition of scaled delta functions
+    for source in P:
+        p_i = source[0]
+        x_i = source[1:]
+        
+        # Calculate the time delay for the source
+        tau_i = np.linalg.norm(x_i) / c
+        
+        # Add the impulse to the RIR
+        h_t += p_i * delta_impulse(t, tau_i, fs)
+    
+    return t, h_t
+
+def getFromFile(index, filename):
+    data = sp.io.loadmat(filename)
+    RIRs = data['RIRs']
+    DOA = RIRs['DOA'][0][index]
+    P = RIRs['P'][0][index]
+    positions = []
+    pressures = []
+    for i in range(50):
+        positions.append([DOA[i][0], DOA[i][1], DOA[i][2]])
+        pressures.append(abs(P[i]))
+    return [{'position': tuple(pos), 'pressure': pressure} for pos, pressure in zip(positions, pressures)]
+
+
+
+
+# positions = np.random.rand(num_sources, 3) * 5  # random positions within a 5x5x5 cube
+# pressures = np.random.rand(num_sources)  # random pressures between 0 and 1
+# return [{'position': tuple(pos), 'pressure': pressure} for pos, pressure in zip(positions, pressures)]
---
+++ interpolation_test.py
@@ -0,0 +1,37 @@
+import SPAIdermenLib as SPAI
+import matplotlib.pyplot as plt
+
+
+
+P = SPAI.getFromFile(0, 'room_impulse_responses.mat')
+Q = SPAI.getFromFile(4, 'room_impulse_responses.mat')
+R = SPAI.getFromFile(2, 'room_impulse_responses.mat')
+print(P)
+# P= SPAI.generate_random_sources(10)
+# Q= SPAI.generate_random_sources(10)
+kappa = 0.5
+
+P_positions, Q_positions = SPAI.calculate_cloud_position(P, R)
+
+T = SPAI.transport_plan(P_positions, Q_positions)
+
+SPAI.visualize_interpolation(P, R, T, kappa)
+
+# Example usage:
+# Set the speed of sound to 343 m/s (speed of sound in air at 20 degrees Celsius)
+# and a sampling rate of 44100 Hz (standard for audio)
+speed_of_sound = 343
+sampling_rate = 44100
+max_simulation_time = 0.5  # in seconds
+
+X_1 = interpolated_point_cloud = SPAI.construct_interpolated_point_cloud(P_positions, Q_positions, T, kappa)
+
+t1, h_t1 = SPAI.calculate_omnidirectional_RIR(X_1, speed_of_sound, sampling_rate, max_simulation_time)
+
+# Plot the RIR
+plt.figure()
+plt.plot(t1, h_t1)
+plt.xlabel('Time [s]')
+plt.ylabel('Amplitude')
+plt.title('Simulated Omnidirectional RIR')
+plt.show()
